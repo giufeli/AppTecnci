f4b888fb91369a655e248d47928a11f0eb54b0dc
Giulio Felicetti <108230280+giufeli@users.noreply.github.com>
2026-08-05 16:49:52 +0200

    Update

--- AppTecnici.Client/Program.cs
+++ AppTecnici.Client/Program.cs
@@ -1,15 +1,24 @@
 using AppTecnici.Client;
+using AppTecnici.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 using Radzen;
 using Blazored.LocalStorage;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// LocalStorage
 builder.Services.AddBlazoredLocalStorage();
 
-// Configurazione HttpClient con l'URL corretto del Server
+// Supporto Autenticazione Blazor
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+// Configurazione HttpClient con l'URL del Server
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7248/")
@@ -17,4 +26,5 @@ builder.Services.AddScoped(sp => new HttpClient
 
 // Registrazione componenti Radzen
 builder.Services.AddRadzenComponents();
+
 await builder.Build().RunAsync();
---
+++ AppTecnici.Client/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace AppTecnici.Client.Services
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private const string AUTH_KEY = "is_authenticated";
+        private const string USERNAME_KEY = "auth_username";
+
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var isAuth = await _localStorage.GetItemAsync<bool>(AUTH_KEY);
+            var username = await _localStorage.GetItemAsync<string>(USERNAME_KEY);
+
+            if (!isAuth || string.IsNullOrEmpty(username))
+            {
+                var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+                return new AuthenticationState(anonymous);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            }, "CustomAuth");
+
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+
+        public async Task MarkUserAsAuthenticated(string username)
+        {
+            await _localStorage.SetItemAsync(AUTH_KEY, true);
+            await _localStorage.SetItemAsync(USERNAME_KEY, username);
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            }, "CustomAuth");
+
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public async Task MarkUserAsLoggedOut()
+        {
+            await _localStorage.RemoveItemAsync(AUTH_KEY);
+            await _localStorage.RemoveItemAsync(USERNAME_KEY);
+
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+        }
+    }
+}
